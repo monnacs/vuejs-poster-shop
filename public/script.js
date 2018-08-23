6d2f9b9c4ae67c9992cbea6f2eef52f050a2f1a7
Monna Cleide <monnacs@gmail.com>
2018-08-23 14:19:06 -0300

    Sections 1-4 done

--- public/script.js
+++ public/script.js
@@ -1,1 +1,62 @@
-console.log('It works.');
+// Variables in caps are usually constants;
+var PRICE = 9.99;
+
+new Vue({
+  el: '#app',
+  data: {
+    total: 0,
+    items: [
+      { id: 1, title: 'Item 1' },
+      { id: 2, title: 'Item 2' },
+      { id: 3, title: 'Item 3' }
+    ],
+    cart: []
+  },
+  methods: {
+    addItem: function(index) {
+      this.total += PRICE;
+      var item = this.items[index];
+      var found = false;
+
+      for (var i = 0; i < this.cart.length; i++) {
+        if (this.cart[i].id === item.id) {
+          found = true;
+          this.cart[i].quantity++;
+          break;
+        }
+      }
+
+      if(!found) {
+        this.cart.push({
+          id: item.id,
+          title: item.title,
+          quantity: 1,
+          price: PRICE
+        });
+      }
+    },
+    inc: function(item) {
+      item.quantity++;
+      this.total += PRICE;
+    },
+    dec: function(item) {
+      item.quantity--;
+      this.total -= PRICE;
+
+      if (item.quantity <= 0) {
+        for (var i = 0; i < this.cart.length; i++) {
+          if (this.cart[i].id === item.id) {
+            this.cart.splice(i, 1);
+            break;
+          }
+        }
+      }
+    }
+  },
+  filters: {
+    currency: function(price) {
+      //toFixed fixes decimals to a certain number
+      return 'R$ '.concat(price.toFixed(2));
+    }
+  }
+});
